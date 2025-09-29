ece53194455c57e87b03ddd0b796bec12889c852
Serafeim Dossas <serafeimdossas@gmail.com>
2025-09-29 21:21:52 +0300

    initialized react and node apps

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+// define api port
+const PORT = process.env.BACKEND_PORT || 8000;
+
+// parse JSON
+app.use(express.json());
+
+// test init route
+app.get("/", (_req, res) => {
+  res.send("Express API init!");
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
