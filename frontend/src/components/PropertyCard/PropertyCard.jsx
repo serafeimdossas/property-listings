a5e797a7f1dc9cfa5996ccfd1cc4b58886711098
Serafeim Dossas <serafeimdossas@gmail.com>
2025-10-04 19:30:29 +0300

    attributes in property card

--- frontend/src/components/PropertyCard/PropertyCard.jsx
+++ frontend/src/components/PropertyCard/PropertyCard.jsx
@@ -1,9 +1,26 @@
 import placeholderImg from "./../../assets/placeholder-image.png";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import StairsOutlinedIcon from "@mui/icons-material/StairsOutlined";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+import BedOutlinedIcon from "@mui/icons-material/BedOutlined";
+import ShowerOutlinedIcon from "@mui/icons-material/ShowerOutlined";
+import HandymanOutlinedIcon from "@mui/icons-material/HandymanOutlined";
 import "./PropertyCard.css";
 
 function PropertyCard(props) {
-  const { title, area, type, description, price } = props.property;
+  const {
+    title,
+    area,
+    type,
+    description,
+    price,
+    floor_level,
+    square_meters,
+    bedrooms,
+    bathrooms,
+    year_built,
+    furnished,
+  } = props.property;
 
   const formatPrice = (value) => {
     return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
@@ -23,6 +40,38 @@ function PropertyCard(props) {
           <span>{area}</span>
         </p>
         <p className="property-card-description">{description}</p>
+        <p className="property-card-attributes">
+          {floor_level ? (
+            <div className="property-attribute">
+              <StairsOutlinedIcon fontSize="small" />
+              <span>{floor_level}</span>
+            </div>
+          ) : null}
+          {square_meters ? (
+            <div className="property-attribute">
+              <FullscreenIcon fontSize="small" />
+              <span>{`${square_meters}m²`}</span>
+            </div>
+          ) : null}
+          {bedrooms ? (
+            <div className="property-attribute">
+              <BedOutlinedIcon fontSize="small" />
+              <span>{bedrooms}</span>
+            </div>
+          ) : null}
+          {bathrooms ? (
+            <div className="property-attribute">
+              <ShowerOutlinedIcon fontSize="small" />
+              <span>{bathrooms}</span>
+            </div>
+          ) : null}
+          {year_built ? (
+            <div className="property-attribute">
+              <HandymanOutlinedIcon fontSize="small" />
+              <span>{year_built}</span>
+            </div>
+          ) : null}
+        </p>
         <p className="property-card-price">{`${formatPrice(price)} €`}</p>
       </div>
     </div>
